84cc0f19d5a11654c66c7bd69da3312333e2e701
Bari Cok <neon03-uu@yandex.ru>
2019-10-11 21:36:12 +0300

    add include style

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name="home"),
 ]
